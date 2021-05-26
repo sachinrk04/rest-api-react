5c1f2a3ee1d062ae77d6912cf2206c6367d25521
Sachin Singh <sachin.innoventes@phonepe.com>
2021-05-26 23:11:55 +0530

    socket-io done

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -8,7 +8,7 @@ import Input from '../../components/Form/Input/Input';
 import Paginator from '../../components/Paginator/Paginator';
 import Loader from '../../components/Loader/Loader';
 import ErrorHandler from '../../components/ErrorHandler/ErrorHandler';
-import './Feed.css';
+import './Feed.css'; 
 
 class Feed extends Component {
   state = {
